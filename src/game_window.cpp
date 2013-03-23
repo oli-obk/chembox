b4f31ebaf7611c4bba8fafbbb3db386dffe31e91
Oliver Schneider <mail@oli-obk.de>
2013-03-23 21:48:38 +0100

    render dragged element

--- src/game_window.cpp
+++ src/game_window.cpp
@@ -131,6 +131,13 @@ void GameWindow::draw()
 	Toolbox.draw();
 	graphics().popTransform();
 	graphics().popTransform();
+	if (dragdrop) {
+		graphics().pushTransform(Gosu::translate(input().mouseX() - gridwdt/double(grid.width())/2, input().mouseY()- gridhgt/double(grid.height())/2));
+		graphics().pushTransform(Gosu::scale(gridwdt/double(grid.width()), gridhgt/double(grid.height())));
+		dragdrop->draw();
+		graphics().popTransform();
+		graphics().popTransform();
+	}
 }
 
 void GameWindow::loseFocus()
