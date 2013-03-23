7920890ba7ccd8810af0d520dcaccab17eff965d
Oliver Schneider <mail@oli-obk.de>
2013-03-23 14:44:54 +0100

    improved builtin_wrapper

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -18,7 +18,23 @@ private:
 	T val;
 public:
 	builtin_wrapper(T v = def):val(v) {}
-	operator T() { return val; }
+	operator T() const { return val; }
+	T& operator+= (const T& rhs)
+	{
+		return val += rhs;
+	}
 };
+
+template<typename T>
+T operator+(const builtin_wrapper<T>& lhs, const T& rhs)
+{
+	return static_cast<T>(lhs) + rhs;
+}
+
+template<typename T>
+T operator+(const T& lhs, const builtin_wrapper<T>& rhs)
+{
+	return lhs + static_cast<T>(rhs);
+}
 
 #endif // CHEMBOX_HELPERS_HPP
