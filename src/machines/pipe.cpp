oli-obk/chembox

commit 2ec6aa97492a2930522cfbc01358b9cdad6387b8

randomize pipe's sending directions to prevent bias

src/machines/pipe.cpp
@@ -23,6 +23,7 @@ void Pipe::Action(size_t id)
 }
 
 std::weak_ptr<Gosu::Font> Pipe::s_pFont;
+std::mt19937 Pipe::engine;
 
 Pipe::Pipe(Gosu::Graphics& g, int dir, size_t version)
 :RotatableVersionedMachine(g, dir, version, L"pipe")
@@ -67,12 +68,15 @@ void Pipe::send()
     // prepare sending buffer
     auto distr = particles.split(connections);
     particles.clear();
+
+    size_t v[] = {0, 1, 2, 3};
+    std::shuffle(std::begin(v), std::end(v), engine);
     // send stuff from buffer
     size_t i = 0;
 	for (ReceiveFromDir dir:{ReceiveFromDir::Up, ReceiveFromDir::Down, ReceiveFromDir::Left, ReceiveFromDir::Right}) {
         auto con = getConnector(dir);
         if (!con) continue;
-		con->push(distr[i++]);
+		con->push(distr[v[i++]]);
 	}
 	particle_engine.update();
 }

src/machines/pipe.hpp
@@ -10,6 +10,7 @@ private:
 	Pipe(const Pipe& rhs);
 	std::shared_ptr<Gosu::Font> m_pFont;
 	static std::weak_ptr<Gosu::Font> s_pFont;
+    static std::mt19937 engine; // Mersenne twister MT19937
 public:
 	ParticleMap particles;
 	Pipe(Gosu::Graphics& g, int dir, size_t version = 0);

commit 9e1f37eaf9dc0e19732e360918c2dfe94e842160

bugfix: only fourway pipe worked
bug introduced by 2ec6aa97492a2930522cfbc01358b9cdad6387b8

src/machines/pipe.cpp
@@ -70,7 +70,7 @@ void Pipe::send()
     particles.clear();
 
     size_t v[] = {0, 1, 2, 3};
-    std::shuffle(std::begin(v), std::end(v), engine);
+    std::shuffle(std::begin(v), v+connections, engine);
     // send stuff from buffer
     size_t i = 0;
 	for (ReceiveFromDir dir:{ReceiveFromDir::Up, ReceiveFromDir::Down, ReceiveFromDir::Left, ReceiveFromDir::Right}) {