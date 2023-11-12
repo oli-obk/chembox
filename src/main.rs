0a21d4692f8c5c6d2472282206b9ebf76ec85859
Oliver Scherer <github@oli-obk.de>
2023-11-12 23:19:47 +0100

    basic bevy setup done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,117 @@
 use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+use bevy_pancam::{PanCam, PanCamPlugin};
+
+const PIPES: &[&str] = &[
+    "empty.png",
+    "drain_pipe.png",
+    "drain_pipe0.png",
+    "drain_pipe2.png",
+    "drain_pipe2opposite.png",
+    "drain_pipe3.png",
+    "drain_pipe4.png",
+    "end_pipe.png",
+    "end_pipe0.png",
+    "end_pipe2.png",
+    "end_pipe2opposite.png",
+    "end_pipe3.png",
+    "end_pipe4.png",
+    "pipe.png",
+    "pipe0.png",
+    "pipe2.png",
+    "pipe2opposite.png",
+    "pipe3.png",
+    "pipe4.png",
+    "pump.png",
+    "pump_spinner.png",
+    "straight_pipe.png",
+    "turn_pipe.png",
+    "t_pipe.png",
+];
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default()).insert(PanCam {
+        grab_buttons: vec![MouseButton::Middle], // which buttons should drag the camera
+        enabled: true,        // when false, controls are disabled. See toggle example.
+        zoom_to_cursor: true, // whether to zoom towards the mouse or the center of the screen
+        min_scale: 1.,        // prevent the camera from zooming too far in
+        ..Default::default()
+    });
+
+    let texture_handle = PIPES.iter().map(|&path| asset_server.load(path)).collect();
+
+    let map_size = TilemapSize { x: 32, y: 32 };
+
+    // Create a tilemap entity a little early.
+    // We want this entity early because we need to tell each tile which tilemap entity
+    // it is associated with. This is done with the TilemapId component on each tile.
+    // Eventually, we will insert the `TilemapBundle` bundle on the entity, which
+    // will contain various necessary components, such as `TileStorage`.
+    let tilemap_entity = commands.spawn_empty().id();
+
+    // To begin creating the map we will need a `TileStorage` component.
+    // This component is a grid of tile entities and is used to help keep track of individual
+    // tiles in the world. If you have multiple layers of tiles you would have a tilemap entity
+    // per layer, each with their own `TileStorage` component.
+    let mut tile_storage = TileStorage::empty(map_size);
+
+    // Spawn the elements of the tilemap.
+    // Alternatively, you can use helpers::filling::fill_tilemap.
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    ..Default::default()
+                })
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
+
+    let tile_size = TilemapTileSize { x: 500.0, y: 500.0 };
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::Square;
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Vector(texture_handle),
+        tile_size,
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+        ..Default::default()
+    });
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(PanCamPlugin::default())
+        .add_plugins(TilemapPlugin)
+        .add_systems(Startup, startup)
+        .add_systems(Update, camera)
         .run();
+}
+
+fn camera(
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    windows: Query<&Window>,
+    mut gizmos: Gizmos,
+) {
+    let (camera, camera_transform) = camera_query.single();
+
+    let Some(cursor_position) = windows.single().cursor_position() else {
+        return;
+    };
+
+    // Calculate a world position based on the cursor's position.
+    let Some(point) = camera.viewport_to_world_2d(camera_transform, cursor_position) else {
+        return;
+    };
+
+    gizmos.circle_2d(point, 10., Color::WHITE);
 }
